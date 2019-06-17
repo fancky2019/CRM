2df2e7f12640dc32e2dd19102e5e1f965d71b1ef
fancky <709737588@qq.com>
2019-06-17 18:11:09 +0800

    登录

--- CRM/FrmLgoin.Designer.cs
+++ CRM/FrmLgoin.Designer.cs
@@ -28,95 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.tePassword = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.ceRememberPassword = new DevExpress.XtraEditors.CheckEdit();
             this.sbtnLogin = new DevExpress.XtraEditors.SimpleButton();
-            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.teUserName = new DevExpress.XtraEditors.TextEdit();
             this.lblMessage = new DevExpress.XtraEditors.LabelControl();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tePassword.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ceRememberPassword.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teUserName.Properties)).BeginInit();
             this.SuspendLayout();
             // 
-            // textEdit1
+            // tePassword
             // 
-            this.textEdit1.Location = new System.Drawing.Point(110, 109);
-            this.textEdit1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Properties.PasswordChar = '*';
-            this.textEdit1.Size = new System.Drawing.Size(125, 24);
-            this.textEdit1.TabIndex = 0;
+            this.tePassword.EditValue = "123456";
+            this.tePassword.Location = new System.Drawing.Point(96, 85);
+            this.tePassword.Name = "tePassword";
+            this.tePassword.Properties.PasswordChar = '*';
+            this.tePassword.Size = new System.Drawing.Size(109, 20);
+            this.tePassword.TabIndex = 0;
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(59, 71);
+            this.labelControl1.Location = new System.Drawing.Point(52, 55);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(45, 18);
+            this.labelControl1.Size = new System.Drawing.Size(36, 14);
             this.labelControl1.TabIndex = 1;
             this.labelControl1.Text = "账号：";
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(59, 112);
+            this.labelControl2.Location = new System.Drawing.Point(52, 87);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(45, 18);
+            this.labelControl2.Size = new System.Drawing.Size(36, 14);
             this.labelControl2.TabIndex = 2;
             this.labelControl2.Text = "密码：";
             // 
             // ceRememberPassword
             // 
-            this.ceRememberPassword.Location = new System.Drawing.Point(110, 140);
+            this.ceRememberPassword.Location = new System.Drawing.Point(96, 109);
+            this.ceRememberPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ceRememberPassword.Name = "ceRememberPassword";
             this.ceRememberPassword.Properties.Caption = "记住密码";
-            this.ceRememberPassword.Size = new System.Drawing.Size(94, 22);
+            this.ceRememberPassword.Size = new System.Drawing.Size(82, 19);
             this.ceRememberPassword.TabIndex = 3;
             // 
             // sbtnLogin
             // 
-            this.sbtnLogin.Location = new System.Drawing.Point(110, 179);
+            this.sbtnLogin.Location = new System.Drawing.Point(96, 139);
+            this.sbtnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.sbtnLogin.Name = "sbtnLogin";
-            this.sbtnLogin.Size = new System.Drawing.Size(94, 29);
+            this.sbtnLogin.Size = new System.Drawing.Size(82, 23);
             this.sbtnLogin.TabIndex = 4;
             this.sbtnLogin.Text = "登录";
             this.sbtnLogin.Click += new System.EventHandler(this.SbtnLogin_Click);
             // 
-            // textEdit2
+            // teUserName
             // 
-            this.textEdit2.Location = new System.Drawing.Point(110, 68);
-            this.textEdit2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textEdit2.Name = "textEdit2";
-            this.textEdit2.Size = new System.Drawing.Size(125, 24);
-            this.textEdit2.TabIndex = 5;
+            this.teUserName.EditValue = "admin";
+            this.teUserName.Location = new System.Drawing.Point(96, 53);
+            this.teUserName.Name = "teUserName";
+            this.teUserName.Size = new System.Drawing.Size(109, 20);
+            this.teUserName.TabIndex = 5;
             // 
             // lblMessage
             // 
-            this.lblMessage.Location = new System.Drawing.Point(110, 43);
+            this.lblMessage.Appearance.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Appearance.Options.UseForeColor = true;
+            this.lblMessage.Location = new System.Drawing.Point(96, 33);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(0, 18);
+            this.lblMessage.Size = new System.Drawing.Size(0, 14);
             this.lblMessage.TabIndex = 6;
             // 
             // FrmLgoin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(319, 238);
+            this.ClientSize = new System.Drawing.Size(279, 185);
             this.Controls.Add(this.lblMessage);
-            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.teUserName);
             this.Controls.Add(this.sbtnLogin);
             this.Controls.Add(this.ceRememberPassword);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.labelControl1);
-            this.Controls.Add(this.textEdit1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Controls.Add(this.tePassword);
             this.MaximizeBox = false;
             this.Name = "FrmLgoin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tePassword.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ceRememberPassword.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teUserName.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,12 +130,12 @@
 
         #endregion
 
-        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit tePassword;
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.CheckEdit ceRememberPassword;
         private DevExpress.XtraEditors.SimpleButton sbtnLogin;
-        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit teUserName;
         private DevExpress.XtraEditors.LabelControl lblMessage;
     }
 }
--- CRM/FrmLgoin.cs
+++ CRM/FrmLgoin.cs
@@ -1,4 +1,6 @@
-﻿using DevExpress.XtraEditors;
+﻿using CRM.BLL.SystemManager;
+using CRM.Model.EntityModels;
+using DevExpress.XtraEditors;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,14 +15,38 @@ namespace CRM
 {
     public partial class FrmLgoin : XtraForm
     {
+        UserBll _userBll = null;
         public FrmLgoin()
         {
             InitializeComponent();
+            _userBll = new UserBll();
         }
 
         private void SbtnLogin_Click(object sender, EventArgs e)
         {
-            if(this.ceRememberPassword.Checked)
+            if(string.IsNullOrEmpty( this.teUserName.Text.Trim()))
+            {
+                this.lblMessage.Text = "账号不能为空！";
+                return;
+            }
+            if (string.IsNullOrEmpty(this.tePassword.Text.Trim()))
+            {
+                this.lblMessage.Text = "密码不能为空！";
+                return;
+            }
+
+            User user = _userBll.GetUser(this.teUserName.Text.Trim());
+            if(user==null)
+            {
+                this.lblMessage.Text = "账号不存在！";
+                return;
+            }
+            if(user.Password!=this.tePassword.Text.Trim())
+            {
+                this.lblMessage.Text = "密码错误！";
+                return;
+            }
+            if (this.ceRememberPassword.Checked)
             {
 
             }
