534bca327f321f4cf934002a1b2810e5c8df6337
fancky <709737588@qq.com>
2019-06-18 18:32:01 +0800

    菜单整理

---
+++ CRM/UserControls/StatisticsReport/CheckOutStatistics.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CRM.UserControls.MemberManager
+{
+    partial class CheckOutStatistics
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(19, 171);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(70, 14);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "labelControl1";
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(113, 168);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(481, 20);
+            this.textEdit2.TabIndex = 3;
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(507, 209);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(87, 27);
+            this.simpleButton2.TabIndex = 5;
+            this.simpleButton2.Text = "simpleButton2";
+            // 
+            // ConsumeStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.textEdit2);
+            this.Name = "ConsumeStatistics";
+            this.Size = new System.Drawing.Size(610, 303);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+    }
+}
---
+++ CRM/UserControls/StatisticsReport/CheckOutStatistics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace CRM.UserControls.MemberManager
+{
+    /// <summary>
+    /// 积分兑换
+    /// </summary>
+    public partial class CheckOutStatistics : XtraUserControl
+    {
+        public CheckOutStatistics()
+        {
+            InitializeComponent();
+        }
+    }
+}
