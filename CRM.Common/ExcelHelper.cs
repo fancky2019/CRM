ed5b4a37db1e03728683f78ddced75b7f7cadf28
fancky <709737588@qq.com>
2019-06-22 16:52:37 +0800

    excel

---
+++ CRM.Common/ExcelHelper.cs
@@ -0,0 +1,124 @@
+﻿using log4net;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Common
+{
+    public class ExcelHelper
+    {
+        public static ExcelHelper Instance { get; set; }
+        ILog logger;
+        static ExcelHelper()
+        {
+            Instance = new ExcelHelper();
+        }
+        private ExcelHelper()
+        {
+            logger = LogManager.GetLogger(typeof(ExcelHelper));
+        }
+
+        public DataSet ExcelToDataSet(string filePath)
+        {
+            //bool hasTitle = true;
+            string fileType = System.IO.Path.GetExtension(filePath);
+            if (string.IsNullOrEmpty(fileType)) return null;
+            DataSet ds = new DataSet();
+            //string strCon = string.Format("Provider=Microsoft.ACE.OLEDB.{0}.0;" +
+            //               "Extended Properties=\"Excel {1}.0;HDR={2};IMEX=1;\";" +
+            //               "data source={3};",
+            //               (fileType == ".xls" ? 4 : 12), (fileType == ".xls" ? 8 : 12), (hasTitle ? "Yes" : "NO"), filePath);
+
+            string strCon = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1;\";data source={0};", filePath);
+
+
+            using (OleDbConnection myConn = new OleDbConnection(strCon))
+            {
+                myConn.Open();
+                System.Data.DataTable table = myConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string tableName = table.Rows[0]["Table_Name"].ToString();
+                string strExcel = $"select * from [{ tableName }]";
+                OleDbDataAdapter myCommand = new OleDbDataAdapter(strExcel, myConn);
+                myCommand.Fill(ds);
+            }
+            if (ds == null || ds.Tables.Count <= 0) return null;
+            return ds;
+        }
+
+        public void SaveDataToExcelFile<T>(List<T> list, string filePath = "D:\\1.xls")
+        {
+            object misValue = System.Reflection.Missing.Value;
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Workbook xlWorkBook = xlApp.Workbooks.Add(misValue);
+            Worksheet xlWorkSheet = (Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            //PropertyInfo[] props = typeof(Sales).GetProperties(BindingFlags.Public);
+            //for (int i = 0; i < props.Length; i++)
+            //{
+            //    xlWorkSheet.Cells[1, i + 1] = props[i].Name; //write the column name
+            //}
+            List<string> descriptionStrList = GetTypeDescriptions(typeof(T));
+            //列头
+            for (int i = 0; i < descriptionStrList.Count; i++)
+            {
+                xlWorkSheet.Cells[1, i + 1] = descriptionStrList[i]; //write the column name
+            }
+
+            //填充数据
+            PropertyInfo[] props = typeof(T).GetProperties();
+            for (int i = 0; i < list.Count; i++)
+            {
+                //填写属性数据
+                for (int j = 0; j < props.Length; j++)
+                {
+
+                    xlWorkSheet.Cells[i + 2, j + 1] = props[j].GetValue(list[i]);
+
+                }
+                ////到时候用反射取property
+                //xlWorkSheet.Cells[i + 2, 1] = studentList[i].Saler;
+                //xlWorkSheet.Cells[i + 2, 2] = studentList[i].SaleCount;
+                //xlWorkSheet.Cells[i + 2, 3] = studentList[i].SalerAchievement;
+                //xlWorkSheet.Cells[i + 2, 4] = studentList[i].TeamName;
+                //xlWorkSheet.Cells[i + 2, 5] = studentList[i].TeamCount;
+                //xlWorkSheet.Cells[i + 2, 6] = studentList[i].TeamSaleAchievement;
+            }
+            //设置列宽自适应
+            xlWorkSheet.Columns.AutoFit();
+            //对齐方式
+            xlWorkSheet.Columns.HorizontalAlignment = XlHAlign.xlHAlignLeft;//文本对齐
+            xlWorkSheet.Name = "职业顾问部门信息";
+            try
+            {
+                xlWorkBook.SaveAs(filePath, XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlWorkBook.Close(true, misValue, misValue);
+                xlApp.Quit();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString());
+            }
+
+        }
+
+        private List<string> GetTypeDescriptions(Type type)
+        {
+            List<string> descriptionStrList = new List<string>();
+            Type descriptionType = typeof(DescriptionAttribute);
+            foreach (PropertyInfo info in type.GetProperties())
+            {
+                DescriptionAttribute des = (DescriptionAttribute)info.GetCustomAttribute(descriptionType);
+                descriptionStrList.Add(des.Description);
+            }
+            return descriptionStrList;
+        }
+
+    }
+}
---
+++ CRM.Common/XMLHelper.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace MingMing.Common
+{
+    public class XMLHelper
+    {
+
+        //XmlSerializer
+        public static XMLHelper Instance { get; set; }
+        static XMLHelper()
+        {
+            Instance = new XMLHelper();
+        }
+
+        private XMLHelper()
+        {
+
+        }
+
+        /// <summary>
+        /// 创建xml文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="fileName"></param>
+        public void CreateXML(string path, string fileName)
+        {
+            if (File.Exists(Path.Combine(path, fileName)))
+            {
+                File.Delete(Path.Combine(path, fileName));
+            }
+
+            //if (!File.Exists(Path.Combine(path, fileName)))
+            //{
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(dec);
+            //根节点  
+            XmlElement root = doc.CreateElement("Department");
+            doc.AppendChild(root);
+            doc.Save("Department.xml");
+            //}
+        }
+
+        /// <summary>
+        /// 写入xml数据
+        /// </summary>
+        /// <param name="teamList"></param>
+        //public void WriteDatas(List<Team> teamList)
+        //{
+        //    XmlDocument xmlDocument = new XmlDocument();
+        //    string path = Path.Combine(Application.StartupPath, "Department.xml");
+        //    xmlDocument.Load(path);
+        //    XmlNode root = xmlDocument.SelectSingleNode("Department");
+        //    foreach (Team item in teamList)
+        //    {
+        //        XmlNode node = xmlDocument.CreateNode("element", "Data", "");
+        //        //ID
+        //        XmlElement idElement = xmlDocument.CreateElement("ID");
+        //        idElement.InnerText = item.ID.ToString();
+        //        node.AppendChild(idElement);
+
+        //        //ParentID
+        //        XmlElement parentIDElement = xmlDocument.CreateElement("ParentID");
+        //        parentIDElement.InnerText = item.ParentID.ToString();
+        //        node.AppendChild(parentIDElement);
+
+        //        //销售小组
+        //        XmlElement teamNameElement = xmlDocument.CreateElement("TeamName");
+        //        teamNameElement.InnerText = item.TeamName;
+        //        node.AppendChild(teamNameElement);
+
+        //        //置业顾问
+        //        XmlElement salerElement = xmlDocument.CreateElement("Saler");
+        //        salerElement.InnerText = item.Saler;
+        //        node.AppendChild(salerElement);
+
+        //        root.AppendChild(node);
+        //    }
+        //    xmlDocument.Save(path);
+        //}
+
+        /// <summary>
+        /// 读取xml
+        /// </summary>
+        /// <returns></returns>
+        //public List<Team> ReadXMLData()
+        //{
+        //    string path = Path.Combine(Application.StartupPath, "Department.xml");
+        //    if (!File.Exists(Path.Combine(path)))
+        //    {
+        //        return null;
+        //    }
+
+        //    List<Team> teamList = new List<Team>();
+
+        //    XDocument doc = XDocument.Load(path);
+        //    IEnumerable<XElement> node = doc.Element("Department").Elements("Data");
+
+        //    foreach (XElement element in node)
+        //    {
+        //        Team team = new Team();
+        //        team.ID = int.Parse(element.Elements("ID").FirstOrDefault().Value);
+        //        team.ParentID = int.Parse(element.Elements("ParentID").FirstOrDefault().Value);
+        //        team.TeamName = element.Elements("TeamName").FirstOrDefault().Value;
+        //        team.Saler = element.Elements("Saler").FirstOrDefault().Value;
+        //        teamList.Add(team);
+
+        //    }
+        //    return teamList;
+
+        //}
+
+        ///// <summary>
+        ///// 插入
+        ///// </summary>
+        ///// <param name="team"></param>
+        ///// <returns></returns>
+        //public int Insert(Team team)
+        //{
+        //    string path = Path.Combine(Application.StartupPath, "Department.xml");
+        //    XDocument doc = XDocument.Load(path);
+        //    XElement node = doc.Element("Department").Elements().Where(p => string.IsNullOrEmpty(p.Element("Saler").Value)).First();
+
+        //    //XElement node = rootNode.Element("Department").Elements("Data").Where(p =>string.IsNullOrEmpty( p.Element("Saler").Value)).First();
+        //    XElement element = new XElement("Data",
+        //          new XElement("ID", team.ID),
+        //          new XElement("ParentID", team.ParentID),
+        //          new XElement("TeamName", team.TeamName),
+        //          new XElement("Saler", team.Saler)
+        //         );
+        //    node.AddBeforeSelf(element);
+        //    doc.Save(path);
+        //    return 1;
+        //}
+
+        //public Team GetXmlNodeByID(int id)
+        //{
+        //    string path = Path.Combine(Application.StartupPath, "Department.xml");
+        //    XDocument doc = XDocument.Load(path);
+        //    XElement element = doc.Element("Department").Elements("Data").Where(p => p.Element("ID").Value == id.ToString()).FirstOrDefault();
+        //    Team team = new Team();
+        //    team.ID = int.Parse(element.Elements("ID").First().Value);
+        //    team.ParentID = int.Parse(element.Elements("ParentID").First().Value);
+        //    team.TeamName = element.Elements("TeamName").First().Value;
+        //    team.Saler = element.Elements("Saler").First().Value;
+        //    return team;
+        //}
+
+        ///// <summary>
+        ///// 修改
+        ///// </summary>
+        ///// <param name="team"></param>
+        ///// <returns></returns>
+        //public int AlterTeam(Team team)
+        //{
+        //    string path = Path.Combine(Application.StartupPath, "Department.xml");
+        //    XDocument doc = XDocument.Load(path);
+
+        //    IEnumerable<XElement> t = doc.Element("Department").Elements("Data").Where(p => p.Element("ID").Value == team.ID.ToString());
+
+        //    XElement elementParentID = t.FirstOrDefault().Elements("ParentID").FirstOrDefault();
+        //    elementParentID.Value = team.ParentID.ToString();
+
+        //    XElement elementTeamName = t.FirstOrDefault().Elements("TeamName").FirstOrDefault();
+        //    elementTeamName.Value = team.TeamName;
+
+        //    XElement elementSaler = t.FirstOrDefault().Elements("Saler").FirstOrDefault();
+        //    elementSaler.Value = team.Saler;
+
+        //    doc.Save(path);
+        //    return 1;
+        //}
+
+        /// <summary>
+        /// 删除节点
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int DeleteNode(int id)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Department.xml");
+            XDocument doc = XDocument.Load(path);
+
+            IEnumerable<XElement> t = doc.Element("Department").Elements("Data").Where(p => p.Element("ID").Value == id.ToString());
+            t.Remove();
+
+            doc.Save(path);
+            return 1;
+        }
+
+
+
+        ///// <summary>
+        ///// 获取部门节点
+        ///// </summary>
+        ///// <param name="teamName"></param>
+        ///// <returns></returns>
+        //public Team GetTeamByTeamName(string teamName)
+        //{
+        //    string path = Path.Combine(Application.StartupPath, "Department.xml");
+        //    XDocument doc = XDocument.Load(path);
+
+        //    XElement element = doc.Element("Department").Elements().Where(p => string.IsNullOrEmpty(p.Element("Saler").Value) && (p.Element("ParentID").Value == "0") && p.Element("TeamName").Value == teamName).First();
+        //    Team team = new Team();
+        //    team.ID = int.Parse(element.Elements("ID").First().Value);
+        //    team.ParentID = int.Parse(element.Elements("ParentID").First().Value);
+        //    team.TeamName = element.Elements("TeamName").First().Value;
+        //    team.Saler = element.Elements("Saler").First().Value;
+        //    return team;
+        //}
+
+
+        ///// <summary>
+        ///// 节点后添加部门节点
+        ///// </summary>
+        ///// <param name="team"></param>
+        ///// <returns></returns>
+        //public int AddTeamNode(Team team)
+        //{
+        //    string path = Path.Combine(Application.StartupPath, "Department.xml");
+
+
+        //    List<Team> teamList = new List<Team>();
+
+        //    XDocument doc = XDocument.Load(path);
+        //    IEnumerable<XElement> node = doc.Element("Department").Elements("Data");
+        //    //List<int> idStrList = node.Select(p =>int.Parse( p.Element("ID").Value)).ToList();
+        //    int maxID = node.Select(p => int.Parse(p.Element("ID").Value)).Max(p => p);
+        //    team.ID = maxID + 1;
+
+        //    XElement lastElement = node.Last();
+        //    XElement element = new XElement("Data",
+        //  new XElement("ID", team.ID),
+        //  new XElement("ParentID", team.ParentID),
+        //  new XElement("TeamName", team.TeamName),
+        //  new XElement("Saler", team.Saler)
+        // );
+        //    lastElement.AddAfterSelf(element);
+        //    doc.Save(path);
+
+        //    return 1;
+        //}
+
+        /// <summary>
+        /// 检查要添加的部门是否存在
+        /// </summary>
+        /// <param name="teamName"></param>
+        /// <returns></returns>
+        public bool CheckTeamNameExist(string teamName)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Department.xml");
+            XDocument doc = XDocument.Load(path);
+
+            IEnumerable<XElement> teamElement = doc.Element("Department").Elements().Where(p => string.IsNullOrEmpty(p.Element("Saler").Value) && (p.Element("ParentID").Value == "0") && p.Element("TeamName").Value == teamName);
+
+            return teamElement.Count() > 0;
+        }
+
+        /// <summary>
+        /// 删除部门
+        /// </summary>
+        /// <param name="id">部门节点ID</param>
+        /// <returns></returns>
+        public int DeleteTeamNode(string teamName)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Department.xml");
+            XDocument doc = XDocument.Load(path);
+
+            //XElement teamElement = doc.Element("Department").Elements().Where(p => string.IsNullOrEmpty(p.Element("Saler").Value) && (p.Element("ParentID").Value == "0") && p.Element("ID").Value == id.ToString()).First();
+            //IEnumerable<XElement> salerElement = doc.Element("Department").Elements().Where(p => string.IsNullOrEmpty(p.Element("Saler").Value) && (p.Element("ParentID").Value == "0") && p.Element("ParentID").Value == id.ToString());
+
+            IEnumerable<XElement> departmentElement = doc.Element("Department").Elements().Where(p => p.Element("TeamName").Value == teamName);
+            departmentElement.Remove();
+            doc.Save(path);
+
+            return 1;
+        }
+    }
+}
