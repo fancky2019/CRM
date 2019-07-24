5d47854646f5daecbc7f25734aa0c82092525cbe
fancky <709737588@qq.com>
2019-07-24 15:35:47 +0800

    日志大小分割

--- CRM/FrmLog4NetTest.Designer.cs
+++ CRM/FrmLog4NetTest.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.btnInfo = new System.Windows.Forms.Button();
             this.btnDebug = new System.Windows.Forms.Button();
+            this.btnFileSize = new System.Windows.Forms.Button();
+            this.btnBufferCount = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnInfo
@@ -52,11 +54,33 @@
             this.btnDebug.UseVisualStyleBackColor = true;
             this.btnDebug.Click += new System.EventHandler(this.BtnDebug_Click);
             // 
+            // btnFileSize
+            // 
+            this.btnFileSize.Location = new System.Drawing.Point(175, 170);
+            this.btnFileSize.Name = "btnFileSize";
+            this.btnFileSize.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSize.TabIndex = 2;
+            this.btnFileSize.Text = "file2K";
+            this.btnFileSize.UseVisualStyleBackColor = true;
+            this.btnFileSize.Click += new System.EventHandler(this.BtnFileSize_Click);
+            // 
+            // btnBufferCount
+            // 
+            this.btnBufferCount.Location = new System.Drawing.Point(421, 160);
+            this.btnBufferCount.Name = "btnBufferCount";
+            this.btnBufferCount.Size = new System.Drawing.Size(75, 23);
+            this.btnBufferCount.TabIndex = 3;
+            this.btnBufferCount.Text = "buffer30";
+            this.btnBufferCount.UseVisualStyleBackColor = true;
+            this.btnBufferCount.Click += new System.EventHandler(this.BtnBufferCount_Click);
+            // 
             // FrmLog4NetTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBufferCount);
+            this.Controls.Add(this.btnFileSize);
             this.Controls.Add(this.btnDebug);
             this.Controls.Add(this.btnInfo);
             this.Name = "FrmLog4NetTest";
@@ -69,5 +93,7 @@
 
         private System.Windows.Forms.Button btnInfo;
         private System.Windows.Forms.Button btnDebug;
+        private System.Windows.Forms.Button btnFileSize;
+        private System.Windows.Forms.Button btnBufferCount;
     }
 }
